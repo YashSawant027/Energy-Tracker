7bdd824435e8c62d22aea77de4161ee9da711a99
YashSawant027 <yashsawan70@gmail.com>
2025-05-16 01:17:00 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Nav from './Nav/Nav';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
 import Products from './cpmponents/Products';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-  let productss = createBrowserRouter([
+  let productss = createHashRouter([
     {
     path: '/',
     element: <App />
